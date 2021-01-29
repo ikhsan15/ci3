44dff814e1314c6d7b468cbdef489d621f69678b
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-01-29 16:35:02 +0700

    update

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -33,59 +33,43 @@ class Welcome extends CI_Controller {
     $config['max_size']             = 10000;
     $config['max_width']            = 10000;
     $config['max_height']           = 10000;
-
     $this->load->library('upload', $config);
 
-    if ( ! $this->upload->do_upload('foto'))
-    {
-      echo "gagal upload foto";
-      // print_r($config);
-    }
-    else
-    {
-      // $data = array('upload_data' => $this->upload->data());
-      // $this->load->view('upload_success', $data);
       $foto = $this->upload->data();
       $foto = $foto['file_name'];
-      $nama = $this->input->post('nama');
-      $tmp_lahir = $this->input->post('tmp_lahir');
-      $tgl_lahir = $this->input->post('tgl_lahir');
-      $gender = $this->input->post('gender');
-      $no_hp = $this->input->post('no_hp');
-      $email = $this->input->post('email');
 
       $DataInsert = array(
-        'nama' => $nama,
-        'tmp_lahir' => $tmp_lahir,
-        'tgl_lahir' => $tgl_lahir,
-        'gender' => $gender,
-        'no_hp' => $no_hp,
-        'email' => $email,
-        'foto' => $foto,
+        'nama' => $this->input->post('nama'),
+        'tmp_lahir' => $this->input->post('tmp_lahir'),
+        'tgl_lahir' => $this->input->post('tgl_lahir'),
+        'gender' => $this->input->post('gender'),
+        'no_hp' => $this->input->post('no_hp'),
+        'email' => $this->input->post('email'),
       );
       $this->M_Pegawai->InsertDataPgw($DataInsert);
       redirect (base_url('Welcome'));
-    }
+
+      // print_r($config);
   }
   public function AksiEdit(){
     $id = $this->input->post('id');
-    $nama = $this->input->post('nama');
-    $tmp_lahir = $this->input->post('tmp_lahir');
-    $tgl_lahir = $this->input->post('tgl_lahir');
-    $gender = $this->input->post('gender');
-    $no_hp = $this->input->post('no_hp');
-    $email = $this->input->post('email');
-    $foto = $this->input->post('foto');
-
+    
     $DataUpdate = array(
-      'nama' => $nama,
-      'tmp_lahir' => $tmp_lahir,
-      'tgl_lahir' => $tgl_lahir,
-      'gender' => $gender,
-      'no_hp' => $no_hp,
-      'foto' => $foto,
+      'nama' => $this->input->post('nama'),
+      'tmp_lahir' => $this->input->post('tmp_lahir'),
+      'tgl_lahir' => $this->input->post('tgl_lahir'),
+      'gender' => $this->input->post('gender'),
+      'no_hp' => $this->input->post('no_hp'),
+      'email' => $this->input->post('email'),
     );
-
+    if('foto' != null){
+      $DataUpdate = array(
+        'foto' => $this->input->post('foto'),
+      );
+    }else{
+      // 'foto' => $this->input->post('foto')
+      $foto = base_url(assets/images/$data_pgw->foto);
+    }
     $this->M_Pegawai->EditDataPgw($DataUpdate, $id);
     redirect (base_url());
   }
